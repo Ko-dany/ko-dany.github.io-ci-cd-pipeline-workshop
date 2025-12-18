490224696f010a5b58fa8de7f1c5c4d6ece6166e
Ko-dany <iamgo910@gmail.com>
2025-12-18 18:02:57 -0500

    Add test cases and test step in pipeline

---
+++ src/main.test.js
@@ -0,0 +1,12 @@
+import { describe, it, expect } from "vitest";
+
+describe("Simple Test", () => {
+  it("should pass basic math", () => {
+    expect(1 + 1).toBe(2);
+  });
+
+  it("should check if app title exists", () => {
+    const title = "CI/CD Workshop";
+    expect(title).toBeTruthy();
+  });
+});
